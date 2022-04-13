2093f444380e4bd94b6b0e6a4dde3cf6426b9af4
Kaunain Ahmad <kaunain.ahmad2000@gmail.com>
2022-04-13 16:49:06 +0530

    updated

--- pages/contact.js
+++ pages/contact.js
@@ -2,8 +2,9 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline bg-slate-100 text-slate-400">
-        Hello world! Testing
+        Contact Us
       </h1>
+      <h2>Hello </h2>
     </>
   );
 }
